e5d757339ce4b240d10d9e4c78c962c5967ccc26
ordros <reordros@gmail.com>
2016-06-02 03:15:18 +0900

    remove debug code

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import time
 import cv2
 import socket
 
-
 ALIGN = {
         "I":[3, 5],
         "Z":[3, 3],
@@ -18,26 +17,19 @@ ALIGN = {
         "I2":[2, 4]
         }
 
-
-
 # move to capture_bombriss.py
 if __name__ == '__main__':
     c = capture_bombliss.CaptureBombliss()
-    #t = think_bombriss.ThinkBombriss(boardsize = (capture_bombriss.WINDOW_POS[3]/capture_bombriss.CHIP_X, capture_bombriss.WINDOW_POS[2]/capture_bombriss.CHIP_Y))
     t = think_bombliss.ThinkBombliss(boardsize=(10,22))
 
     c.parse_next()
-#    c.parse_chips(cv2.imread("screenshot.png"))
-#    c.gen_board_img()
-#    w = c.get_current_mino()
-#    print t.think(whitning(c.board), "L"),":",w
 
     server = "192.168.10.148"
     port = 50000
 
-    #s = ControlBombliss(0)
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     soc.connect((server, port))
+
 
     while True:
         c.parse_next()
@@ -48,12 +40,10 @@ if __name__ == '__main__':
             c.next_flag = 0
             #c.gen_board_img()
             print "Next!"
-            #c.parse_chips(cv2.imread("screenshot.png"))
             choice = t.think(c.whitning_board(c.board), w)
             print choice,":",w
             al = ALIGN[w][int(choice[1])]
             # xpos, sel, ypos, align
-            print al
             soc.send(str(choice[0])+"_"+str(choice[1])+"_"+str(choice[2])+"_"+str(al))
 
         c.current_mino = c.next_mino
