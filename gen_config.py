4a557caa6af5792cb3cde165b83ab847ea5f998a
ordros <reordros@gmail.com>
2016-06-23 19:21:11 +0900

    make gen_config.py to generate config_bombliss.py from yaml

---
+++ gen_config.py
@@ -0,0 +1,76 @@
+import yaml
+
+class generateConfig:
+    def __init__(self, config_file):
+        fl = open(config_file)
+        self.data = yaml.load(fl)
+        self.load_config()
+        fp = open(self.data["tetrimino_pattern"]).read()
+        self.TETRIMINOS = eval(fp)
+
+    def load_config(self):
+        data = self.data
+        al = {}
+        tetriminos = []
+        cnt = 0
+        for d in data["tetriminos"].items():
+            al.update({d[0]:d[1]["align"]})
+            tetriminos.append(d[0])
+            cnt += 1
+        self.ALIGN = al
+        self.NEXT_MINOS = tetriminos
+        self.chip_size = data["puzzle_chip"]["size"]
+
+    def gen_configfile(self):
+        f = open("config_bombliss.py", "w")
+        f.writelines("NEXT_MINOS = "+str(self.NEXT_MINOS)+"\n")
+        f.writelines("WINDOW_POS = "+str(self.WINDOW_POS)+"\n")
+        f.writelines("NEXT_POS = "+str(self.NEXT_POS)+"\n")
+        f.writelines("NEXT_IMGS = "+str(self.NEXT_IMGS)+"\n")
+        f.writelines("CHIP_X = "+str(self.chip_size[0])+"\n")
+        f.writelines("CHIP_Y = "+str(self.chip_size[1])+"\n")
+        f.writelines("ALIGN = "+str(self.ALIGN)+"\n")
+        f.writelines("TETRIMINOS = "+str(self.TETRIMINOS)+"\n")
+        f.close()
+
+    @property
+    def NEXT_MINOS(self):
+        return self.NEXT_MINOS
+
+    @property
+    def CHIP_X(self):
+        return self.chip_size[0]
+
+    @property
+    def CHIP_Y(self):
+        return self.chip_size[1]
+
+    @property
+    def ALIGN(self):
+        return self.ALIGN
+
+    @property
+    def WINDOW_POS(self):
+        return self.data["window"]["board_position"] + self.data["window"]["board_size"]
+
+    @property
+    def NEXT_POS(self):
+        return self.data["window"]["next_position"] + self.data["window"]["next_size"]
+
+    @property
+    def NEXT_IMGS(self):
+        return [self.data["directory_next_imgs"]+x+"_binary.png" for x in self.NEXT_MINOS]
+
+if __name__ == "__main__":
+    c = generateConfig(config_file = "config_bombliss_snes.yaml")
+    c.load_config()
+    c.gen_configfile()
+    """
+    print c.NEXT_MINOS
+    print c.WINDOW_POS
+    print c.NEXT_POS
+    print c.NEXT_IMGS
+    print c.CHIP_X, c.CHIP_Y
+    print c.ALIGN
+    #print c.TETRIMINOS
+    """
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import gen_config
+g = gen_config.generateConfig(config_file="config_bombliss_snes.yaml")
+g.gen_configfile()
 import BomblissMan
 
 server = "192.168.10.148"
