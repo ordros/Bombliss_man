2ea72a921a791161853b0a39590fb6e92c000657
ordros <reordros@gmail.com>
2016-06-24 04:36:50 +0900

    update controL_bombliss.py

--- control_bombliss.py
+++ control_bombliss.py
@@ -5,17 +5,21 @@ import time
 import socket
 import threading
 
-#GPIO number
+
 RIGHT = 22
 LEFT = 4
 DOWN = 17
+UP = 18
 A = 27
+B = 15
+R = 23
+SEL = 14
 
-PINS = [RIGHT, LEFT, DOWN, A]
-PINS_STR = ["R", "L", "D", "A"]
+PINS = [RIGHT, LEFT, DOWN, A, B, UP, R, SEL]
+PINS_STR = ["R", "L", "D", "A", "B", "UP", "R", "S"]
 
+#wait = 0.02
 wait = 0.02
-
 host = "192.168.10.148"
 port = 50000
 
@@ -31,14 +35,14 @@ def press(sel):
        	time.sleep(wait)
         GPIO.output(PINS[sel], False)
 	time.sleep(wait)
-	#print "Press: " + PINS_STR[sel]
+	#print "Press: " + PINS_STR[sel] 
 	#print PINS_STR[sel]
 
 def move(xpos, ypos, sel):
 	for i in xrange(sel):
 		press(3)
 	time.sleep(0.05)
-
+	
 	if xpos < 0 :
 		for i in xrange(abs(xpos)):
 			press(1)
@@ -61,7 +65,7 @@ def down(d):
 	#	press(2)
 	#	time.sleep(0.02)
 
-
+	
 def main():
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     	sock.bind((host, port))
@@ -78,17 +82,17 @@ def main():
 		print "recv.", msg
 		try:
 			a = msg.split("_")
-			if a :
+			if a : 
 				xpos = int(a[0])
 				sel = int(a[1])
 				ypos = int(a[2])
 				align = int(a[3])
 				print xpos-align, ypos, sel
 				move(xpos-align, ypos, sel)
-
+				
 		except Exception as e:
 			print str(e)
-
+			
 			GPIO.cleanup()
 			sock.close()
 			break
@@ -99,7 +103,7 @@ def main():
 if __name__ == "__main__":
 	init()
 	while 1:
-		comm = raw_input("0:right, 1:left, 2:down, 3:A, g:start -> ")
+		comm = raw_input("0:right, 1:left, 2:down, 3:A, 4:B, 5:UP, 6:R, 7:SELECT, g:start -> ")
         	if comm == "g":
                 	main()
                 	break
